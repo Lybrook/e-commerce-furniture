dafa172b9b065cc6802531bc0f67ccabf18446bf
Lybrook <omoshlybrook@gmail.com>
2025-02-03 11:45:50 +0300

    feat: update routing to include CreateProduct page and comment out Orders route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
-import Orders from "./pages/Orders";
+// import Orders from "./pages/Orders";
+import CreateProduct from "./pages/CreateProduct";
 import "./index.css";
 
 export default function App() {
@@ -13,7 +14,8 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<Products />} />
-          <Route path="/orders" element={<Orders />} />
+          {/* <Route path="/orders" element={<Orders />} /> */}
+          <Route path="/create-product" element={<CreateProduct />} />
         </Routes>
       </div>
     </Router>
